eb7305de4ef03c4c0bc4efd90bc2e5a3d1a72466
dome-sieb <sieberdominique10@gmail.com>
2024-06-26 10:46:26 +0200

    Firebase installiert

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:dachdecker_app/firebase_options.dart';
 import 'package:dachdecker_app/src/data/database_repository.dart';
 import 'package:dachdecker_app/src/data/mock_database.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   DatabaseRepository databaseRepository = MockDatabase();
   databaseRepository.init();
   runApp(App(databaseRepository));
